7483147075d4fed10f2ba03a57b139a2f4162c22
alastair38 <alastair@alastaircox.com>
2024-03-12 13:08:49 +0000

    + deployed site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-import metaTags from "astro-meta-tags";
+import metaTags from 'astro-meta-tags';
 
-import tunnel from "astro-tunnel";
+import tunnel from 'astro-tunnel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://psychedelic-ethics.vercel.app',
   i18n: {
     defaultLocale: 'en',
     locales: ['es', 'en', 'fr', 'es-419', 'nl', 'de', 'pt-BR'],
     routing: {
-      prefixDefaultLocale: false
-    }
+      prefixDefaultLocale: false,
+    },
   },
-  integrations: [tailwind(), icon(), metaTags(), tunnel()]
+  integrations: [tailwind(), icon(), metaTags(), tunnel()],
 });
