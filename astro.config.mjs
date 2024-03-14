778f9405f40cbeca393b8a5d2a9e007fd4c3e112
alastair38 <alastair@alastaircox.com>
2024-03-14 11:20:22 +0000

    ~ renamed base layout - old imports etc + robots.txt set to disallow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import metaTags from 'astro-meta-tags';
-
 import tunnel from 'astro-tunnel';
+
+// npm dist of sitemap does not currently work with i18n configuration if locale route contains numbers. Schema.ts https://github.com/withastro/astro/blob/main/packages/integrations/sitemap/src/schema.ts regex needs '\d' added after the '-'. Uncomment i18n config to build before deployment and add built sitemaps to public folder. Also uncomment sitemap link in SEO component. Then comment out i18n config to prevent sitemap build error when deploying
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +17,24 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
-  integrations: [tailwind(), icon(), metaTags(), tunnel()],
+  integrations: [
+    tailwind(),
+    icon(),
+    metaTags(),
+    tunnel(),
+    // sitemap({
+    //   i18n: {
+    //     defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+    //     locales: {
+    //       en: 'en', // The `defaultLocale` value must present in `locales` keys
+    //       es: 'es',
+    //       fr: 'fr',
+    //       'es-419': 'es-419',
+    //       nl: 'nl',
+    //       de: 'de',
+    //       'pt-BR': 'pt-br',
+    //     },
+    //   },
+    // }),
+  ],
 });
